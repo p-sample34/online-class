d0e56f7e64100ccb778ac45000751da327187a85
p-sample34 <psample34@gmail.com>
2025-11-05 10:42:02 +0530

    Update student.js

--- routes/student.js
+++ routes/student.js
@@ -2,9 +2,15 @@ const express = require("express");
 const router = express.Router();
 const Answer = require("../models/Answer");
 
-router.post("/submit-answer", async (req, res) => {
-  const ans = await Answer.create(req.body);
-  res.json(ans);
+router.post("/answer", async (req, res) => {
+  const { studentName, answer } = req.body;
+  
+  if (!studentName || !answer) {
+    return res.json({ success: false });
+  }
+
+  await Answer.create({ studentName, answer });
+  res.json({ success: true });
 });
 
 module.exports = router;
